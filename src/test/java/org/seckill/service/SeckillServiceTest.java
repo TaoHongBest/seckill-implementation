93d692f627bac1610e0d7b5de0338159cc1b3256
NOTpanda <alexander113@126.com>
2018-09-11 16:41:05 +0100

    JUnit test completed. All passed.

---
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -0,0 +1,70 @@
+package org.seckill.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author taohong on 11/09/2018
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml"})
+public class SeckillServiceTest {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @Test
+    public void getSeckillList() throws Exception {
+//    public void getSeckillList() {
+        List<Seckill> list = seckillService.getSeckillList();
+        logger.info("list={}", list);
+    }
+
+    @Test
+    public void getById() throws Exception {
+//    public void getById() {
+        long id = 1000;
+        Seckill seckill = seckillService.getById(id);
+        logger.info("seckill={}", seckill);
+
+    }
+
+    // Complete logic of integrated testing code, pay attention to the repeatable execution
+    @Test
+    public void testSeckillLogic() throws Exception {
+        long id = 1001;
+        Exposer exposer = seckillService.exportSeckillUrl(id);
+        if (exposer.isExposed()) {
+            long phone = 32345678901L;
+            String md5 = exposer.getMd5();
+            try {
+                SeckillExecution seckillExecution = seckillService.executeSeckill(id, phone, md5);
+                logger.info("result={}", seckillExecution);
+            } catch (RepeatKillException e) {
+                logger.error(e.getMessage());
+            } catch (SeckillCloseException e) {
+                logger.error(e.getMessage());
+            }
+        } else {
+            // Seckill has not started yet
+            logger.warn("exposer={}", exposer);
+
+        }
+    }
+}
