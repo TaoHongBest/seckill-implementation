c3165896d717640a5ee1bc7e3962af9db99ce240
NOTpanda <alexander113@126.com>
2018-09-11 16:26:35 +0100

    Add annotation to manage transactions

--- src/main/java/org/seckill/dto/Exposer.java
+++ src/main/java/org/seckill/dto/Exposer.java
@@ -19,6 +19,18 @@ public class Exposer {
     // End time
     private long end;
 
+    @Override
+    public String toString() {
+        return "Exposer{" +
+                "exposed=" + exposed +
+                ", md5='" + md5 + '\'' +
+                ", seckillId=" + seckillId +
+                ", now=" + now +
+                ", start=" + start +
+                ", end=" + end +
+                '}';
+    }
+
     public Exposer(boolean exposed, String md5, long seckillId) {
         this.exposed = exposed;
         this.md5 = md5;
