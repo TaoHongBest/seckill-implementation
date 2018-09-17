eba3174bb7d7d47680a956d8787604d1cc00e5a1
NOTpanda <alexander113@126.com>
2018-09-17 13:20:40 +0100

    Web interaction implemented.

--- src/main/webapp/WEB-INF/jsp/detail.jsp
+++ src/main/webapp/WEB-INF/jsp/detail.jsp
@@ -12,13 +12,71 @@
             <h1>${seckill.name}</h1>
         </div>
         <div class="panel-body">
+            <h2 class="text-danger">
+                <%-- Display time icon --%>
+                <span class="glyphicon glyphicon-time"></span>
+                <%-- Display countdown --%>
+                <span class="glyphicon" id="seckill-box"></span>
+            </h2>
+        </div>
+    </div>
+</div>
+<%-- Prompt out login window, requiring entering phone number --%>
+<div id="killPhoneModal" class="modal fade">
+
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title text-center">
+                    <span class="glyphicon glyphicon-phone"> </span>秒杀电话:
+                </h3>
+            </div>
+
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-xs-8 col-xs-offset-2">
+                        <input type="text" name="killPhone" id="killPhoneKey"
+                               placeholder="填写手机号^o^" class="form-control">
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <%-- Validate info --%>
+                <span id="killPhoneMessage" class="glyphicon"> </span>
+                <button type="button" id="killPhoneBtn" class="btn btn-success">
+                    <span class="glyphicon glyphicon-phone"></span>
+                    Submit
+                </button>
+            </div>
+
         </div>
     </div>
 
 </div>
+
 </body>
-<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<%-- jQuery file. Must be imported before bootstrap.min.js --%>
 <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
-<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+<%-- Latest Bootstrap core JavaScript file --%>
 <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<%-- Use CND to acquire public js: http://www.bootcdn.cn/ --%>
+<%-- jQuery Cookie operation plug-in --%>
+<script src="https://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+<%-- jQuery countDown plug-in --%>
+<script src="https://cdn.bootcss.com/jquery.countdown/2.2.0/jquery.countdown.min.js"></script>
+
+<script src="/resources/script/seckill.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+    $(function () {
+        // Use El expression to transfer data
+        seckill.detail.init({
+            seckillId:${seckill.seckillId},
+            startTime:${seckill.startTime.time}, // millisecond
+            endTime:${seckill.endTime.time}
+        });
+    });
+</script>
 </html>
--- src/main/webapp/resources/script/seckill.js
+++ src/main/webapp/resources/script/seckill.js
@@ -2,88 +2,148 @@
 // javascript modularity
 // seckill.detail.init(params);
 var seckill = {
-        // Package the urls of sekill-related ajax
-        URL: {
-            now: function () {
-                return '/seckill/time/now';
-            }
+    // Package the urls of sekill-related ajax
+    URL: {
+        now: function () {
+            return '/seckill/time/now';
         },
-        // Validate phone number
-        validatePhone: function (phone) {
-            if (phone && phone.length == 11 && !isNaN(phone)) {
-                return true;
-            } else {
-                return false;
-            }
+        exposer: function (seckillId) {
+            return '/seckill/' + seckillId + '/exposer';
         },
+        execution: function (seckillId, md5) {
+            return '/seckill/' + seckillId + '/' + md5 + '/execution';
 
-        countdown: function (seckillId, nowTime, startTime, endTime) {
-            var seckillBox = $('#seckill-box');
-            // Time determination
-            if (nowTime > endTime) {
-                // seckill ends
-                seckillBox.html('Seckill End!');
-            } else if (nowTime < startTime) {
-                // seckill hasn't started, bind countdown event
-                var killTime = new Date(startTime + 1000);
-                seckillBox.countdown(killTime, function (event) {
-                    // Time format
-                    var format = event.strftime('Seckill Countdown: %D days %H hours %M mins %S secs');
-                    seckillBox.html(format);
-                });
-            } else {
-            }
-        },
+        }
+    },
 
-        // Logic of detail page
-        detail:
-            {
-                // Initialize the detail page
-                init: function (params) {
-                    // Phone number validation and login, time interaction
-                    // Planning for the interactive processes
-                    // Search phone number in cookie
-                    var killPhone = $.cookie('killPhone');
-
-                    if (!seckill.validatePhone(killPhone)) {
-                        // Bind phone number
-                        // Control output
-                        var killPhoneModal = $('#killPhoneModal');
-                        killPhoneModal.modal({
-                            // Display prompt out layer
-                            show: true,
-                            backdrop: 'static', // Forbid closing the location
-                            keyboard: false // Close keyboard event
-                        });
-                        $('#killPhoneBtn').click(function () {
-                            var inputPhone = $('#killPhoneKey').val();
-                            console.log('inputPhone=' + inputPhone); //TODO
-                            if (seckill.validatePhone(inputPhone)) {
-                                // write phone number to cookie
-                                $.cookie('killPhone', inputPhone, {expires: 7, path: '/seckill'});
-                                // Refresh page
-                                window.location.reload();
-                            } else {
-                                $('#killPhoneMessage').hide().html('<label class="label label-danger">Invalid Phone Number!</label>').show(300);
+    // Acquire seckill address, control displaying logic, execute seckill
+    handleSeckillKill: function (seckillId, node) {
+        node.hide().html('<button class="btn btn-primary btn-lg" id="killBtn">Start to seckill!</button>');
+        $.post(seckill.URL.exposer(seckillId), {}, function (result) {
+            // In recursed method, execute the interaction process
+            if (result && result['success']) {
+                var exposer = result['data'];
+                if (exposer['exposed']) {
+                    // Seckill starts
+                    // Acquire seckill address
+                    var md5 = exposer['md5'];
+                    var killUrl = seckill.URL.execution(seckillId, md5);
+                    console.log("killUrl" + killUrl);
+                    // Bind click event only one time
+                    $('#killBtn').one('click', function () {
+                        // Execute seckill request
+                        // 1: Disable the button
+                        $(this).addClass('disabled');
+                        // 2: Send seckill request
+                        $.post(killUrl, {}, function (result) {
+                            if (result && result['success']) {
+                                var killResult = result['data'];
+                                var state = killResult['state'];
+                                var stateInfo = killResult['stateInfo'];
+                                // 3: Display seckill result
+                                node.html('<span class="label label-success">' + stateInfo + '</span>');
                             }
                         });
-                    }
-                    // Have logged in
-                    // Countdown interaction
-                    var startTime = params['startTime'];
-                    var endTime = params['endTime'];
-                    var seckillId = params['seckillId'];
-                    $.get(seckill.URL.now(), {}, function (result) {
-                        if (result && result['success']) {
-                            var nowTime = result['data'];
-                            // Time determination, timing interaction
-                            seckill.countdown(seckillId, nowTime, startTime, endTime);
-                        } else {
-                            console.log('result:' + result);
-                        }
                     });
+                    node.show();
+                } else {
+                    // Seckill hasn't started
+                    var now = exposer['now'];
+                    var start = exposer['start'];
+                    var end = exposer['end'];
+                    // Re-start countdown by the given times
+                    seckill.countdown(seckillId, now, start, end);
                 }
+
+
+            } else {
+                console.log('result:' + result);
             }
+
+
+        });
+    },
+
+    // Validate phone number
+    validatePhone: function (phone) {
+        if (phone && phone.length == 11 && !isNaN(phone)) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+
+    countdown: function (seckillId, nowTime, startTime, endTime) {
+        var seckillBox = $('#seckill-box');
+        // Time determination
+        if (nowTime > endTime) {
+            // seckill ends
+            seckillBox.html('Seckill End!');
+        } else if (nowTime < startTime) {
+            // seckill hasn't started, bind countdown event
+            var killTime = new Date(startTime + 1000);
+            seckillBox.countdown(killTime, function (event) {
+                // Time format
+                var format = event.strftime('Seckill Countdown: %D days %H hours %M mins %S secs');
+                seckillBox.html(format);
+            }).on('finish.countdown', function () {
+                seckill.handleSeckillKill(seckillId, seckillBox);
+            });
+        } else {
+            // seckill starts
+            seckill.handleSeckillKill(seckillId, seckillBox);
+        }
+    },
+
+    // Logic of detail page
+    detail: {
+        // Initialize the detail page
+        init: function (params) {
+            // Phone number validation and login, time interaction
+            // Planning for the interactive processes
+            // Search phone number in cookie
+            var killPhone = $.cookie('killPhone');
+
+            if (!seckill.validatePhone(killPhone)) {
+                // Bind phone number
+                // Control output
+                var killPhoneModal = $('#killPhoneModal');
+                killPhoneModal.modal({
+                    // Display prompt out layer
+                    show: true,
+                    backdrop: 'static', // Forbid closing the location
+                    keyboard: false // Close keyboard event
+                });
+                $('#killPhoneBtn').click(function () {
+                    var inputPhone = $('#killPhoneKey').val();
+                    console.log('inputPhone=' + inputPhone); //TODO
+                    if (seckill.validatePhone(inputPhone)) {
+                        // write phone number to cookie
+                        $.cookie('killPhone', inputPhone, {expires: 7, path: '/seckill'});
+                        // Refresh page
+                        window.location.reload();
+                    } else {
+                        $('#killPhoneMessage').hide().html('<label class="label label-danger">Invalid Phone Number!</label>').show(300);
+                    }
+                });
+            }
+            // Have logged in
+            // Countdown interaction
+            var startTime = params['startTime'];
+            var endTime = params['endTime'];
+            var seckillId = params['seckillId'];
+            $.get(seckill.URL.now(), {}, function (result) {
+                if (result && result['success']) {
+                    var nowTime = result['data'];
+                    // Time determination, timing interaction
+                    seckill.countdown(seckillId, nowTime, startTime, endTime);
+                } else {
+                    console.log('result:' + result);
+                }
+            });
+        }
     }
-;
+
+};
 
